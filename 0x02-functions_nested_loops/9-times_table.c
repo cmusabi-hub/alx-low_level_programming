e087a1db6b1fb94bdf0a0a688f5224d236cf876d
cmusabi-hub <musabicharles91@gmail.com>
2022-09-28 12:44:37 -0700

    A function that prints the last digit of a number

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,20 +1,26 @@
-#include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
-/*
- *A function that prints the 9 times table
+/**
+ * times_table - Prints the 9 times table, starting with 0.
  */
 
 void times_table(void)
 {
-  int num1, num2;
-  for (num1 = 0; num1 <= 9; num1++ )
-    {
-      for(num2 = 0; num2 <= 9; num2++)
-	{
-	  _putchar( (num1 * num2));
-	  _putchar(',');
-	  _putchar(' ');
-	}
-    }_putchar('\n');
+int a, b, result;
+for (a = 0; a <= 9; a++)
+{
+_putchar('0');
+for (b = 1; b <= 9; b++)
+{
+_putchar(',');
+_putchar(' ');
+result = a * b;
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
 }
