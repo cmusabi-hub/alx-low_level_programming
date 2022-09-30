198bd22d26785515c73d64806d7210edf8ed6fbe
cmusabi-hub <musabicharles91@gmail.com>
2022-09-30 07:45:08 -0700

    A function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,26 +4,29 @@
  * print_times_tables - function that prints the n times table, starting with 0
  *
  * n: is the integer
- * num2: is the second integer
+ * m: is an integer
+ * k: is the second integer
  * mult: is the multiplication of the two integers
  */
 
 void print_times_table(int n)
 {
-	int m, mult = n * m;
+	int m, k, mult;
 	if ( n <= 15 && n >=0 )
-		for (m = 0; m <= n; m++)
+		for (m = 0; m <= n; m++ )
 		{
-			_putchar(' ');
-			if (mult < 10)
+			for (k = 0; k <= n; k++)
 			{
-				_putchar(mult + '0');
-				_putchar(',');
+				_putchar(' ');
+				mult = m * k;
+				if (mult < 10)
+					_putchar(mult + '0');
+					_putchar(',');
+				else
+					_putchar(mult + '0');
+					_putchar((mult % 10) + '0');
 			}
-			else
-				_putchar(mult + '0');
-				_putchar((mult % 10) + '0');
-				_putchar(',');
+			_putchar('\n');
 		}
-	_putchar('\n');
+
 }
