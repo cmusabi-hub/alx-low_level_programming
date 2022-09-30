dc3a5d94d01357d0fd120d1f308a88e6c927bbe2
cmusabi-hub <musabicharles91@gmail.com>
2022-09-30 08:02:08 -0700

    A function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 				{
 					_putchar(mult + '0');
 					_putchar((mult / 10) % 10 + '0');
-					_putchar((mult % 10) + '0')
+					_putchar((mult % 10) + '0');
 				}
 			}
 			_putchar('\n');
