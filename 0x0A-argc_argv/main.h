2e51878af048dc3cefc994b0efdb9969cbc8e1ef
cmusabi-hub <musabicharles91@gmail.com>
2022-09-30 16:37:16 -0700

    A program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
+#endif /*MAIN_H*/
