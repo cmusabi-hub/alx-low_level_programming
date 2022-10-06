398dc44af816df724202ee93328f5f0277d2c5ad
cmusabi-hub <musabicharles91@gmail.com>
2022-10-06 11:21:38 -0700

    Function that prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,6 +16,9 @@ void print_triangle(int size)
 		for (i = 0; i < size; i++)
 		{
 			for ((j = size - i); j > 0; j--)
+			{
+				_putchar(' ');
+			}
 			for (j = 0; j < i; j++)
 			{
 				_putchar('#');
