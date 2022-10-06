9143f516c82b2a7463a8c2be656aa5fbb954a8df
cmusabi-hub <musabicharles91@gmail.com>
2022-10-06 08:39:10 -0700

    Afunction that prints square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,9 +15,9 @@ void print_square(int size)
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (b = 0; b < size; b++)
+			for (b = 1; b <= size; b++)
 			{
 				_putchar('#');
 			}
