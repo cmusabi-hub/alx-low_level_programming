e60dcadc783f7b49a79f2b04a4ae601b65f026ab
cmusabi-hub <musabicharles91@gmail.com>
2022-10-12 23:51:19 -0700

    A function that prints n elements of an array of integers followed by

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,11 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
+		printf(",");
+		printf(" ")	
 	{
 		if (i < n)
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
 	}
 	printf("\n");
 }
