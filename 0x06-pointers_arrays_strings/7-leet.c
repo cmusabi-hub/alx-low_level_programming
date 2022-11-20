fda2bad4f8b02b671d6b5e3dc3378e7160696748
cmusabi-hub <musabicharles91@gmail.com>
2022-11-20 03:19:21 -0800

    A function that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - A function that encodes a string into 1337
+ * @s: string to be encoded
+ * Return: return value of s
+ */
+
+char *leet(char *s)
+{
+	int a = 0, b = 0, c = 0;
+	char letter[] = {'a', 'e', 'o', 't', 'l'};
+	char  num[] = {'4', '3', '0', '7', '1'};
+
+	for (; s[a] != '\0'; a++)
+	{
+		if (s[a] == letter[b] || s[a] == letter[b] - 32)
+		{
+			s[a] =num [b];
+			b++;
+			c++;
+		}
+	}
+	return (s);
+}
