9e4b23f9ea214d887e61237ec25e7ee2140997ca
Charles Musabi <musabicharles91@gmail.com>
2022-12-08 18:34:06 +0300

    A function that locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,10 +16,14 @@ char *_strstr(char *haystack, char *needle)
 		for (j = 0, k = 0; needle[j] != '\0'; j++, k++)
 		{
 			if (haystack[k] != needle[j] || haystack[k] == '\0')
+			{
 				break;
+			}
 		}
 		if (needle[j] == '\0')
+		{
 			return (haystack + i);
+		}
 	}
 	return (NULL);
 }
