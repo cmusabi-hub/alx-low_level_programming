1ea0d00d2618f49c94e540283f2e689bfa2f062b
Charles Musabi <musabicharles91@gmail.com>
2022-12-15 08:52:33 +0300

    Palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns 1 if a string is palindrome and 0 if not palindrone
+ * @s: string tested
+ *
+ * Return: string if palindrome
+ */
+
+int is_palindrome(char *s)
+{
+	if (s != '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
