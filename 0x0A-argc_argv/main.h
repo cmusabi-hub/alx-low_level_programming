cb4a668c343c5ee5ce554980e7f8b1a1e081cc16
Charles Musabi <musabicharles91@gmail.com>
2022-12-24 07:36:02 +0300

    A program that prints its name

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,14 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-
+int _putchar(char c);
 int main(int argc __attribute__((unused)), char *argv[]);
-int main(int argc, char *argv[] __attribute__((unused)));
-int main(int argc, char *argv[]);
-int main(int argc, char *argv[]);
-int main(int argc, char *argv[]);
-int main(int argc, char *argv[]);
 
 #endif /*MAIN_H*/
