a54073932a7882f6fc3851a36cc95ca750ab04e8
Charles Musabi <musabicharles91@gmail.com>
2022-12-30 12:02:34 +0300

    Function that creates array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, and
- * initializes it with a specific char
- * @size: Size of the array
- * @c: Character to insert
- * Return: NULL if size is zero or if it fails
- * pointer to array if everything is normal.
+ * create_array - creates an array of chars and initializes with a specific char
+ * @size: bytes
+ * @c: characters to be initialized
+ * 
+ * Return: NULL if size = 0
+ * Return: a pointer to the array or NULL if it fails
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *array;
-	unsigned int index;
-
-	if (size == 0)
-		return (NULL);
-	array = malloc(sizeof(char) * size);
-
-	if (array == NULL)
-		return (NULL);
-	for (index = 0; index < size; index++)
-		array[index] = c;
-	return (array);
 }
-
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-
-int _putchar(char);
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
@@ -13,4 +11,3 @@ char *argstostr(int ac, char **av);
 char **strtow(char *str);
 
 #endif /*MAIN_H*/
-
