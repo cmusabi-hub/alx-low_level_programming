beaa75a84e817f30f8197652418420097089fe2c
Charles Musabi <musabicharles91@gmail.com>
2023-01-13 06:27:13 +0300

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * @struct dog - structure declaration
+ * @name - name of dog
+ * @age - age of dog
+ * @owner - owner of the dog
+ */
+
 struct dog
 {
 	char *name;
