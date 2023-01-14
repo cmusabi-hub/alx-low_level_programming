5472331576698550274aab95038f77f49a439b66
Charles Musabi <musabicharles91@gmail.com>
2023-01-14 16:10:43 +0300

    Function that creates a new dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to the structure of the dog
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d.name);
+	free(d.age);
+	free(d.owner);
+}
