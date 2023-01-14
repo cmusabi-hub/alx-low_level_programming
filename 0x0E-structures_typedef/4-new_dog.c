5098cbc844036ab086f66fc537cebf2ff5a065fa
Charles Musabi <musabicharles91@gmail.com>
2023-01-14 15:47:58 +0300

    Function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,21 +14,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i = 0, len_n, len_o;
 
+	dog_t *dog;
+	dog = malloc(sizeof(dog_t));
+
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	for (; name[i]; i++)
 		len_n++;
-	name = malloc(sizeof(char) * len_n);
-	new_dog -> name == name;
+	dog -> name = malloc(sizeof(char) * len_n);
+	dog -> name = name;
 
-	new_dog -> age == age;
+	dog -> age = age;
 
 	for (; owner[i]; i++)
 		len_o++;
-	new_dog -> owner == owner;
-	owner = malloc(sizeof(char) * len_o);
-	free(name);
-	free(owner);
-	return (0);
+
+	dog -> owner = malloc(sizeof(char) * len_o);
+
+	dog -> owner = owner;
+
+	return (dog);
+	free(dog);
 
 }
