e9839d10ee860f2fc95147beb1633bb3ff517eba
Charles Musabi <musabicharles91@gmail.com>
2023-01-14 13:26:28 +0300

    Function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i; len_n, len_o;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+}
