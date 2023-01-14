1839d07af463a6f3cbcf08b3daaaee324ed6fe38
Charles Musabi <musabicharles91@gmail.com>
2023-01-14 15:55:21 +0300

    Function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,6 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
+	if (dog == NULL)
+		return(NULL);
+
 	for (; name[i]; i++)
 		len_n++;
 
