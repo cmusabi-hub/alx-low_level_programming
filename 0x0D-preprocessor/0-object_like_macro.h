f7abb02d00862fcf47ca4c73109bef70d2312ed9
Charles Musabi <musabicharles91@gmail.com>
2023-01-18 17:54:14 +0300

    Header file and readme file

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,7 @@
-#ifndef _0_OBJECT_LIKE_MACRO.H
-#define _0_OBJECT_LIKE_MACRO.H
+#ifndef _0_OBJECT_LIKE_MACRO.H_
+#define _0_OBJECT_LIKE_MACRO.H_
 
 #define	SIZE	1024
 
-#endif /*_0_OBJECT_LIKE_MACRO.H*/
+#endif /*_0_OBJECT_LIKE_MACRO.H_*/
 
