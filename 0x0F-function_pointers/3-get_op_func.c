963aae92d434096ed0badc0711739ef84b27feb9
Charles Musabi <musabicharles91@gmail.com>
2023-01-24 19:03:22 +0300

    Function that select correct function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -7,4 +7,15 @@
  * NULL if s does not match
  */
 
-
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{"NULL", NULL},
+	};
+	int i;
+}
