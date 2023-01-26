c90652c1ef7c0ddd7c677dd21c75f262487c1ac1
Charles Musabi <musabicharles91@gmail.com>
2023-01-26 06:44:46 +0300

    Main function that takes arguments

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,10 +15,18 @@ int main (int argc, char *argv[])
 	int  num1, num2, sum;
 	int (*f)(int, int);
 
-	arg[1] = atoi(num1);
-	argv[3] = atoi(num2);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	f = get_op_func(argv[2]);
 
 	sum = f(num1, num2);
+	printf("%d\n", sum);
 
 	return (0);
 }
