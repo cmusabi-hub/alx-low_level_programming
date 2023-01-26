19a37431d06f0f3f17b8567932f30800b1ae9268
Charles Musabi <musabicharles91@gmail.com>
2023-01-26 06:09:43 +0300

    Main function that takes arguments

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+
+/**
+ * main - main function
+ * @num1: first integer
+ * @num2: second integer
+ * @operator: operation to be executed
+ *
+ * Return: result of the arguments
+ */
+
+int main (int argc, char *argv[])
+{
+	int i = 0;
+	char num1, (*operator), num2;
+
+	oprator = 
+
+	for (; i < argc; i++)
+	{
+		arg[1] = atoi(num1);
+		argv[2] = atoi(operator);
+		argv[3] = atoi(num2);
+	}
+	return (num1 operator  num2)
+}
