2d062f66435eda6cb0f2c8b0eed15c7f1d970707
Charles Musabi <musabicharles91@gmail.com>
2023-02-05 22:49:15 +0300

    Function that Characters numbers followed by a new line

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings followed by a new line
+ * @separator: , character
+ * @n: number of strings passed to the function
+ * Return: void. if a separator is Null don't print it
+ * If one string is Null,print nil
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	char _char;
+
+	va_list valist;
+
+	va_start(va_list, n);
+
+	for (i = 0; i < n;)
+	{
+		_char = va_arg(valist, char);
+		printf("%s", _char);
+	}
+}
