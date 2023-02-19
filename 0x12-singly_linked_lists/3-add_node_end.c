428fab4d84ed4ddb25749f853ee4b64301f0d14d
Charles Musabi <musabicharles91@gmail.com>
2023-02-19 16:36:23 +0300

    Function that adds a new node at the end of a list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,4 +4,9 @@
 #include <stdio.h>
 
 /**
- * 
+ * add_node_end - adds node at the end of the list
+ * @head: head of the list
+ * @str: string to be duplicated
+ * Return: address of the new element
+ */
+
