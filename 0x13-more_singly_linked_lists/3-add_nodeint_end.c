890f660abea67a7cb0bd7fbb8cde74ff7da5b601
Charles Musabi <musabicharles91@gmail.com>
2023-02-24 11:35:50 +0300

    Functions that adds a new node at the end of a linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,5 +9,15 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-
+	listint_t *newnode;
+	
+	newnode = malloc(sizeof(listint_t));
+	
+	if (newnode != NULL)
+	{
+		newnode->n = n;
+		newnode->next = NULL;
+		*head = newnode;
+	}
+	return (newnode);
 }
