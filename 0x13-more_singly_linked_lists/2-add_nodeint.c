3014b736b40db2d6a2b9667a2ff1a7bc52e865dd
Charles Musabi <musabicharles91@gmail.com>
2023-02-24 08:54:51 +0300

    Functions that adds a new node to the list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
+/**
+ * add_nodeint - adds node at the beginning of a list.
+ * @n: integer constant to be copied to the list.
+ * Return: addres of the new element of Null if it fails.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+
+}
