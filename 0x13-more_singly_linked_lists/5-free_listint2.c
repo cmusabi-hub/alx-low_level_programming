8ed03ed4684f81c887b39ceaedeebd4273708af2
Charles Musabi <musabicharles91@gmail.com>
2023-02-25 14:41:39 +0300

    Functions that frees  a linked list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * free_listint2 - frees a list
+ * @head: head of the list
+ */
+void free_listint2(listint_t **head)
+{
+
+}
