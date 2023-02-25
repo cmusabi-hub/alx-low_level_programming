a4a82d2f047b8f2038dff2c332ea1b0fe073cc50
Charles Musabi <musabicharles91@gmail.com>
2023-02-25 13:22:09 +0300

    Functions that adds a new node at the end of a linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -28,7 +28,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		while (traverse->next != NULL)
 		{
 			traverse = traverse->next;
-			rtraverse->next = newnode;
+			traverse->next = newnode;
 		}
 	}
 	return (*head);
