521e22e15731cccf0aa2c401e1c90a0305ded6a4
Charles Musabi <musabicharles91@gmail.com>
2023-02-28 11:48:50 +0300

    Functions that inserts new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newnode == NULL)
 		return (NULL);
 	
-	for (; *head != NULL && i < idx; i++ )
+	for (; traverse != NULL && i < idx; i++ )
 	{
 		traverse = traverse->next;
 	}
