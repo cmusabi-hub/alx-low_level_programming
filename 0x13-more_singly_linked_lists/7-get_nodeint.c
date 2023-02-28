47d96c705fb0f37a017c891ab4ac48ea1bb73eb7
Charles Musabi <musabicharles91@gmail.com>
2023-02-28 07:56:39 +0300

    Functions that returns nth node of list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,6 +13,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 	listint_t *traverse;
+	
+	if (head == NULL)
+		return(NULL);
 
 	for (; head != NULL && i < index; i++)
 	{
