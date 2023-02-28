f4ef89247e99fe287cacb2ee11c066b412c9f842
Charles Musabi <musabicharles91@gmail.com>
2023-02-28 09:01:13 +0300

    Functions that returns sum of all data(n) of list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * sum_listint - returns sum of all data(n) of a linked list
+ * @head: poiter to the head
+ * Return: sum of data(n)
+ */
+int sum_listint(listint_t *head)
+{
+	unsigned int i = 0;	
+}
