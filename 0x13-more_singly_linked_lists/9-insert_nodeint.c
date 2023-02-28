a0815566951c5d2036acdfb48fa9c60e9078e099
Charles Musabi <musabicharles91@gmail.com>
2023-02-28 09:58:05 +0300

    Functions that inserts new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position.
+ * @head: pointer to the head of the list.
+ * @idx: index of the list where the new node should be added starts at 0.
+ * Return: address of newnode.
+ * 	   NULL if failed.
+ * 	   if not possible to add new node at idx do not add new node and return NULL.
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+}
