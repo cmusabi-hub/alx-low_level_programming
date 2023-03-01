17c63a54944970236e4b82c2f41af425c82d72a1
Charles Musabi <musabicharles91@gmail.com>
2023-03-01 22:37:15 +0300

    Functions that deletes the node at a specified index of a list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	nextnode = malloc(sizeof(listint_t));
 	if (nextnode == NULL)
-		return (NULL);
+		return (-1);
 	while (i < (index - 1))
 	{
 		if (traverse == NULL || traverse->next == NULL)
