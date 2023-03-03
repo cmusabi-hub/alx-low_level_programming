914dc152cd2a49d1be5f88cb1d0e5ba01bbb24df
Charles Musabi <musabicharles91@gmail.com>
2023-03-03 10:46:22 +0300

    Functions that prints a linked  list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_listint_safe - prints a linked list
+ * @head: pointer to the head
+ * Return: returns number of nodes
+ * 	   or exit status 98 .
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int node = 0;
+	listint_t *traverse = *head;
+
+	if (head == NULL)
+		exit (98);
+
+	while (head)
+	{
+		traverse = traverse->next;
+		node++
+	}
+	return (node);
+}
