70023f4ba307802e7e427ce7f60232e923795735
Charles Musabi <musabicharles91@gmail.com>
2023-04-25 21:49:35 +0300

    Function that prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -29,7 +29,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			return (-1);
 	}
 	if (temp->next != NULL)
-		temp->next->prev = temp->prev->net;
+		temp->next->prev = temp->prev->next;
 	current->next = temp->next;
 	free(temp);
 	return (1);
