fbe28bc6f4a3b544535a1eb31470e7263c9502da
Charles Musabi <musabicharles91@gmail.com>
2023-04-25 21:58:38 +0300

    Function that prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,30 +8,34 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	unsigned int position = 0;
-	dlistint_t *temp = *head, *current;
+        unsigned int position = 0;
+        dlistint_t *temp = *head;
 
-	if (*head == NULL || head == NULL)
-		return (-1);
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(temp);
-		return (1);
-	}
-	for (; temp != NULL && position < (index - 1); position++)
-	{
-		current = temp;
-		temp = temp->next;
-		if (temp == NULL)
-			return (-1);
-	}
-	current->next = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = current;
-	free(temp);
-	return (1);
+        if (head == NULL || *head == NULL)
+                return (-1);
+        if (index == 0)
+        {
+                *head = (*head)->next;
+                if (*head != NULL)
+                        (*head)->prev = NULL;
+                free(temp);
+                return (1);
+        }
+        for (; temp != NULL && position < (index - 1); position++)
+        {
+                temp = temp->next;
+                if (temp == NULL)
+                        return (-1);
+        }
+        if (temp->next == NULL)
+        {
+                temp->prev->next = NULL;
+                free(temp);
+                return (1);
+        }
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        free(temp);
+        return (1);
 }
 
