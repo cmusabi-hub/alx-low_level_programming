3ed262eb2161d16caf7507c1499f259737fa4d29
Charles Musabi <musabicharles91@gmail.com>
2023-04-28 00:11:58 +0300

    Proogram will assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,29 +3,22 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * Return:always return 0
- *
+ * main - main function
+ * @n: integer to be tested
  */
 
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
-if (n > 0)
-
-printf("%d is positive\n", n);
-
-else if (n == 0)
-
-printf("%d is zero\n", n);
-else
-
-printf("%d is negative\n", n);
-
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf ("%d is positive", n);
+	else if (n == 0)
+		printf ("%d is zero", n);
+	else
+		printf ("%d is positive", n);
+	return (0);
 }
+
