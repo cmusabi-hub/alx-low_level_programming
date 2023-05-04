87e424bc37aef69c702536325827a09889e1e395
Charles Musabi <musabicharles91@gmail.com>
2023-05-04 15:51:30 +0300

    A program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,29 +7,27 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 98;i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 99; j++)
 		{
-			for (k = 0; k < 10; k++)
+			if (i != j)
 			{
-				for (l = 1; l < 10; l++)
+				int d1 = i / 10;
+				int d2 = i % 10;
+				int d3 = j / 10;
+				int d4 = j % 10;
+				putchar('0' + d1);
+				putchar('0' + d2);
+				putchar(' ');
+				putchar('0' + d3);
+				putchar('0' + d4);
+				if (i != 98 || j != 99)
 				{
-					if (j != l && j < l && j < k)
-					{
-						putchar('0' + i);
-						putchar('0' + j);
-						putchar(' ');
-						putchar('0' + k);
-						putchar('0' + l);
-						if (i < 10 || j < 9 || k < 10 || l < 10)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
