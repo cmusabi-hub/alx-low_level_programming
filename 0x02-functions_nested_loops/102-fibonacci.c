70eecb053aa204cd4b2ab10aff2a84a9333a1b99
Charles Musabi <musabicharles91@gmail.com>
2023-05-09 11:21:35 +0300

    Program that prints fibonacci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main program
+ * Return: return 0 for success
+ */
+
+int main(void)
+{
+	int a = 0, b = 1, sum;
+
+	while (b < 50)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+	}
+	printf("%d\n", sum);
+}
+
