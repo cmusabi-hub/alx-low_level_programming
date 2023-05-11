e80bd7ef683bdf7b4cc688a7106531735e8dcb8e
Charles Musabi <musabicharles91@gmail.com>
2023-05-11 09:37:30 +0300

    Fizz Buzz program  checks divisibilty of 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Fizz Buzz program
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
+
