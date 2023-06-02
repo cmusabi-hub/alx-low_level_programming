afff700490d615daa603d7b35c76d25ba7fbbeb5
Charles Musabi <musabicharles91@gmail.com>
2023-06-02 10:41:56 +0300

    Program that prints the number of arguments passed into it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,7 +8,10 @@
 
 int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc);
+	if (argc > 1)
+		printf("%d\n", argc - 1);
+	else
+		printf("%d\n", argc - 1);
 	return (0);
 }
 
