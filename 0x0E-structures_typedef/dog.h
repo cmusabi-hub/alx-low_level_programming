be191dad566cbf393c6a6f89225671fd7cf76047
Charles Musabi <musabicharles91@gmail.com>
2023-08-06 16:30:57 +0300

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,5 +14,8 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif /*_DOG_H_*/
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /*_DOG_H_*/
