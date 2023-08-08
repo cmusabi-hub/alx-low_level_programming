0df5cf9d469f2081b474077e5b8a0fcae2bb0090
Charles Musabi <musabicharles91@gmail.com>
2023-08-08 17:32:08 +0300

    Function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,33 +1,51 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * new_dog - new dog created
  * @name: pointer to the name
  * @age: age
  * @owner: owner of the dog
- * return: Null if fails
+ * Return: Pointer to the newly created dog, or NULL if fails
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	dog = malloc(sizeof(dog_t));
+    int index = 0, len_owner = 0, len_name = 0;
+    dog_t *dog;
+    dog = malloc(sizeof(dog_t));
 
-	if (dog == NULL)
-		return (NULL);
-	
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-	dog->name = malloc(sizeof(char) * strlen(name));
-	dog->name = name;
-	dog->age = age;
-	dog->owner = malloc(sizeof(char) * strlen(name));
-	dog->owner = owner;
+    for (; name[index] != '\0'; index++)
+        len_name++;
+    for (; owner[index] != '\0'; index++)
+        len_owner++;
 
-	return (dog);
-	free(dog);
+    if (dog == NULL)
+        return NULL;
+
+    if (name == NULL || age < 0 || owner == NULL) {
+        free(dog);
+        return NULL;
+    }
+
+    dog->name = malloc(sizeof(char) * (len_name + 1));
+    if (dog->name == NULL) {
+        free(dog);
+        return NULL;
+    }
+    dog->name = name;
+
+    dog->owner = malloc(sizeof(char) * (len_owner + 1));
+    if (dog->owner == NULL) {
+        free(dog->name);
+        free(dog);
+        return NULL;
+    }
+    dog->owner = owner;
+
+    dog->age = age;
+
+    return dog;
 }
 
