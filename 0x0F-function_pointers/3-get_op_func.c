cbaade6eb54d3dea9cf73a80ffe39031b170c870
Charles Musabi <musabicharles91@gmail.com>
2023-08-27 10:31:34 +0300

    Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,10 +18,11 @@ int (*get_op_func(char *s))(int, int)
 	 };
 	 int i = 0;
 
-	 for (; ops[i].op; i++)
+	 while (ops[i].op)
 	 {
 		 if (*(ops[i].op) == *s && !*(s + 1))
 			 break;
+		 i++;
 	 }
 	 return (ops[i].f);
 }
