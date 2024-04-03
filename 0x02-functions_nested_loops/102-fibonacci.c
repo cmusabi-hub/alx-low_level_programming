de806346f4d6e7cfba684ea409f21492e6780466
Charles Musabi <musabicharles91@gmail.com>
2024-04-03 18:35:19 +0300

    A program that computes the first 50 fibonacci sequence

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 {
 	unsigned long long int num_1 = 1, num_2 = 2, sum, fibon;
 
-	printf("%llu, %llu", num_1, num_2);
+	printf("%lu, %lu", num_1, num_2);
 	for (fibon = 2; fibon < 50; fibon++)
 	{
 		sum = num_1 + num_2;
 		num_1 = num_2;
 		num_2 = sum;
-		printf(", %llu", sum);
+		printf(", %lu", sum);
 	}
 	printf("\n");
 	return (0);
