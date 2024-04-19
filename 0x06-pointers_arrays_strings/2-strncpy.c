75363316352b0a10c46bfb543935b89b9d49e060
Charles Musabi <musabicharles91@gmail.com>
2024-04-19 10:25:40 +0300

    A function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index = 0;
 
-	if (src)
+	while ( index < n)
 	{
-		while ( index < n)
-		{
-			dest[index] = src[index];
-			index++;
-		}
+		dest[index] = src[index];
+		index++;
 	}
 	dest[index] = '\0';
 	return (dest);
