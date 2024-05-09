1bb8a892145d823141b03deaf43de648626fbd5e
Charles Musabi <musabicharles91@gmail.com>
2024-05-09 11:40:47 +0300

    A program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - main function
+ * Return: 0 for success
+ */
+int main(int __attribute__((unused))argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
+
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
+int main(int argc, char *argv[]);
 
-#ifndef _MAIN_H_
+#endif /*_MAIN_H_*/
