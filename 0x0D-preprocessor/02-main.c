988bc9aefc29e503b31f9dfdc603ff5e3d5b410c
Charles Musabi <musabicharles91@gmail.com>
2024-05-26 23:54:05 +0300

    A program that prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
