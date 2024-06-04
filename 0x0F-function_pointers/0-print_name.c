ac52c5cab9b363e5a6f79c59c3a1060ff3dca35e
Charles Musabi <musabicharles91@gmail.com>
2024-06-04 13:32:42 +0300

    Headar file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+/**
+ * print_name: prints name
+ * @name: name to be printed
+ * @f: function pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
+
