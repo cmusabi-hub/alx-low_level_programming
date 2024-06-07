e59ba0d9f3bf74bb1d2e51ebef5391dbb5c05843
Charles Musabi <musabicharles91@gmail.com>
2024-06-07 23:13:51 +0300

    Function file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+/**
+ * op_add - returns sum of two parameters
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: sum of parameters
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns substraction of two parameters
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: subtraction of parameters
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two parameters
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: multiplication parameters
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two parameters
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: division of parameter
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns remainder of the division of a by b
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
