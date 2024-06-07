ec0f002467d8bbf2765d28ad69f24290eab1151b
Charles Musabi <musabicharles91@gmail.com>
2024-06-08 00:03:28 +0300

    Calculator file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int (*func_ptr)(int, int);
-	int num_1 = atoi(argv[1]), num_2 = atoi(argv[2]), result;
+	int num_1 = atoi(argv[1]), num_2 = atoi(argv[3]), result;
 
 	if (argc != 4)
 	{
