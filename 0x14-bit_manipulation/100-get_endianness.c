b1878a1f21e6989aa16c5896962f119852faa8b4
Charles Musabi <musabicharles91@gmail.com>
2024-07-18 23:23:54 +0300

    A function that checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 for big endian and 1 for little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	char *c;
+
+	x = 1;
+	c = (char *)&x;
+
+	return ((int)*c);
+}
+
